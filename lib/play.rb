86e139d7f12cb3a2986514b4ad780bb58dda65ee
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-06-06 23:37:57 +0000

    Automatically backed up by Learn

---
+++ lib/play.rb
@@ -0,0 +1,28 @@
+class Play
+
+  def initialize 
+  end 
+
+  def start
+    puts "What kind of game do you want to play? 1, 2, or 0?"
+    kind = gets.chomp
+    case kind
+      when kind == 0
+        Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
+        puts "Would you like to play again?"
+        loop
+      when kind == 1
+        Game.new(Players::Human.new("X"), Players::Computer.new("O")).play
+        puts "Would you like to play again?"
+        loop
+      when kind == 2
+        Game.new(Players::Human.new("X"), Players::Human.new("O")).play
+        puts "Would you like to play again?"
+        loop
+      else 
+        puts "That entry is invalid. Please select 1, 2, or 0 players."
+        start
+      end
+    end
+
+  end
